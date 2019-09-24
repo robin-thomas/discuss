68790c97d40ef1d55872b92704bf30e8e13d9bdd
Robin Thomas <robinthomas2591@gmail.com>
2019-09-24 08:27:06 +0800

    Setup react

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hello</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
