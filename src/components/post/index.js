babfb2777ac7ef9872a0fcbf57d0607b4deaa7cd
Robin Thomas <robinthomas2591@gmail.com>
2019-09-25 08:17:29 +0800

    Initial work on post page

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -3,13 +3,16 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 import Header from "../header";
+import Post from "./post";
 
 const Home = props => (
   <div>
     <Header />
     <Container>
       <Row>
-        <Col md="10">Hello</Col>
+        <Col md="10">
+          <Post />
+        </Col>
         <Col md="2">Hello</Col>
       </Row>
     </Container>
--- src/components/post/post/Post.js
+++ src/components/post/post/Post.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-// import Post from "./Post";
+import { Row, Col } from "react-bootstrap";
 
-// import "./Posts.css";
+import Moment from "../../utils/Moment";
+import { DataConsumer } from "../../utils/DataProvider";
+
+import "./Post.css";
 
 const Post = props => {
   const post = {
@@ -15,7 +18,78 @@ const Post = props => {
     timestamp: 1569298544
   };
 
-  return <div className="App-posts">Hello</div>;
+  return (
+    <div className="App-post">
+      <Row>
+        <Col>
+          <div className="App-post">
+            <Row>
+              <Col md="auto" className="App-post-vote text-center pr-0">
+                <div>
+                  <DataConsumer>
+                    {ctx =>
+                      ctx.address ? (
+                        <Row>
+                          <Col className="App-post-vote-vote">▲</Col>
+                        </Row>
+                      ) : (
+                        <Row>
+                          <Col>&nbsp;</Col>
+                        </Row>
+                      )
+                    }
+                  </DataConsumer>
+                  <Row>
+                    <Col className="App-post-vote-count">{post.votes}</Col>
+                  </Row>
+                  <DataConsumer>
+                    {ctx =>
+                      ctx.address ? (
+                        <Row>
+                          <Col className="App-post-vote-vote">▼</Col>
+                        </Row>
+                      ) : null
+                    }
+                  </DataConsumer>
+                </div>
+              </Col>
+              <Col md="11">
+                <Row>
+                  <Col className="App-post-title">
+                    <span>{post.title}</span>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="App-post-content">{post.content}</Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <span className="App-post-details App-post-comment">
+                      {post.comments} comments
+                    </span>
+                    <span className="App-post-details App-post-category">
+                      /d/{post.category}
+                    </span>
+                    <span className="App-post-details">by</span>
+                    <span className="App-post-details App-post-user">
+                      {post.user}
+                    </span>
+                    <span className="App-post-details">
+                      {post.timestamp
+                        ? Moment(
+                            post.timestamp * 1000 /* milliseconds */
+                          ).fromNow()
+                        : ""}
+                    </span>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Post;
